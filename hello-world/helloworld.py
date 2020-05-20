2b7f0134787817f0a80443871f1d8233527753ea
moineepa <er.moineepa@gmail.com>
2020-05-21 00:57:23 +0530

    include hello-world

---
+++ hello-world/helloworld.py
@@ -0,0 +1,11 @@
+
+# This is a single line comment
+
+# This priogram just prints Hello World
+
+print("Hello World")
+
+# Now printing Hello World 10 times
+
+for i in range(1,10):
+    print("Hello World" + str(i))
